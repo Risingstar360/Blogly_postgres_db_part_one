2f125337729bd1c35aa0e1b0a0984d68700eb671
Martin <99287130+Risingstar360@users.noreply.github.com>
2023-07-12 12:50:34 +0100

    Part 2 commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -25,7 +25,7 @@ def home():
 
 @app.route('/user-page')
 def users_list():
-    """all registered users"""
+    """All registered users"""
     with app.app_context():
         users = User.query.order_by(User.last_name, User.first_name).all()
     return render_template('users.html', users=users)
@@ -33,7 +33,7 @@ def users_list():
 
 @app.route('/user-page/new', methods=["GET"])
 def users_new_form():
-    """form to register new user"""
+    """Form to register new user"""
     return render_template('new-user.html')
 
 
@@ -56,12 +56,13 @@ def users_show(user_id):
     """Show selected user details"""
     with app.app_context():
         user = User.query.get_or_404(user_id)
-    return render_template('show-user.html', user=user)
+        posts = user.posts  # Fetch the posts for the user
+    return render_template('show-user.html', user=user, posts=posts)
 
 
 @app.route('/user-page/<int:user_id>/edit')
 def users_edit(user_id):
-    """form to edit a registered user"""
+    """Form to edit a registered user"""
     with app.app_context():
         user = User.query.get_or_404(user_id)
     return render_template('edit-user.html', user=user)
@@ -69,7 +70,7 @@ def users_edit(user_id):
 
 @app.route('/user-page/<int:user_id>/edit', methods=["POST"])
 def users_update(user_id):
-    """form to update an already registered user"""
+    """Form to update an already registered user"""
     with app.app_context():
         user = User.query.get_or_404(user_id)
         user.first_name = request.form['first_name']
@@ -90,6 +91,68 @@ def users_destroy(user_id):
         db.session.commit()
 
     return redirect("/user-page")
+
+
+@app.route('/user-page/<int:user_id>/posts/new', methods=["GET"])
+def posts_new_form(user_id):
+    """Form to create a new post"""
+    with app.app_context():
+        user = User.query.get_or_404(user_id)
+    return render_template('new-post.html', user=user)
+
+
+@app.route('/user-page/<int:user_id>/posts/new', methods=["POST"])
+def posts_new(user_id):
+    """Submit form for new post"""
+    with app.app_context():
+        user = User.query.get_or_404(user_id)
+        title = request.form['title']
+        content = request.form['content']
+        post = Post(title=title, content=content, user=user)
+        db.session.add(post)
+        db.session.commit()
+
+    return redirect(f"/user-page/{user_id}")
+
+
+@app.route('/user-page/<int:user_id>/posts/<int:post_id>')
+def posts_show(user_id, post_id):
+    """Show selected post details"""
+    with app.app_context():
+        post = Post.query.get_or_404(post_id)
+    return render_template('show-post.html', post=post)
+
+
+@app.route('/user-page/<int:user_id>/posts/<int:post_id>/edit')
+def posts_edit(user_id, post_id):
+    """Form to edit a post"""
+    with app.app_context():
+        post = Post.query.get_or_404(post_id)
+    return render_template('edit-post.html', post=post)
+
+
+@app.route('/user-page/<int:user_id>/posts/<int:post_id>/edit', methods=["POST"])
+def posts_update(user_id, post_id):
+    """Form to update a post"""
+    with app.app_context():
+        post = Post.query.get_or_404(post_id)
+        post.title = request.form['title']
+        post.content = request.form['content']
+        db.session.add(post)
+        db.session.commit()
+
+    return redirect(f"/user-page/{user_id}/posts/{post_id}")
+
+
+@app.route('/user-page/<int:user_id>/posts/<int:post_id>/delete', methods=["POST"])
+def posts_destroy(user_id, post_id):
+    """Delete a post"""
+    with app.app_context():
+        post = Post.query.get_or_404(post_id)
+        db.session.delete(post)
+        db.session.commit()
+
+    return redirect(f"/user-page/{user_id}")
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -19,52 +20,28 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.Text, nullable=False)
     last_name = db.Column(db.Text, nullable=False)
-    image_url = db.Column(db.Text, nullable=False, default="holding_image")
+    image_url = db.Column(db.Text, nullable=False, default=holding_image)
+    posts = db.relationship('Post', backref='user',
+                            cascade='all, delete-orphan')
+
+
+class Post(db.Model):
+    """Post"""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     @property
     def full_name(self):
         """Return full name of user."""
-
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.user.first_name} {self.user.last_name}"
 
 
 def connect_db(app):
-
     db.app = app
     db.init_app(app)
-
-
-# """SQLAlchemy models for blogly."""
-
-# from flask_sqlalchemy import SQLAlchemy
-
-# db = SQLAlchemy()
-
-# DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
-
-
-# class User(db.Model):
-#     """Site user."""
-
-#     __tablename__ = "users"
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     first_name = db.Column(db.Text, nullable=False)
-#     last_name = db.Column(db.Text, nullable=False)
-#     image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
-
-#     @property
-#     def full_name(self):
-#         """Return full name of user."""
-
-#         return f"{self.first_name} {self.last_name}"
-
-
-# def connect_db(app):
-#     """Connect this database to provided Flask app.
-
-#     You should call this in your Flask app.
-#     """
-
-#     db.app = app
-#     db.init_app(app)
